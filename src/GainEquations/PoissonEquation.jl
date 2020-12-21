270663e45fd5453d73d91acf4729b2ea662f914c
Simone Carlo Surace <simsurace@gmx.net>
2020-12-21 19:15:12 +0100

    docstring typo

--- src/GainEquations/PoissonEquation.jl
+++ src/GainEquations/PoissonEquation.jl
@@ -1,7 +1,7 @@
 @doc raw"""
     PoissonEquation(h, ensemble) ::GainEquation
 
-Returns a Poisson equation struct  representing the equation ``\nabla\cdot(p\nabla \phi) = - \tilde h``, where ``p`` is a probability density and ``\tilde h = h-\int h p dx``. 
+Returns a Poisson equation struct representing the equation ``\nabla\cdot(p\nabla \phi) = - \tilde h``, where ``p`` is a probability density and ``\tilde h = h-\int h p dx``. 
 The container contains the following fields
 * `:h': ``h`` itself
 * `:positions': an i.i.d. sample from ``p``, represented as a matrix
